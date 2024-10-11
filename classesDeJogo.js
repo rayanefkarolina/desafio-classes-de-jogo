330c74ac7b0b0300c8b92a9bda225e5162a06b21
rayanefkarolina <rayane.karolina@a.ucb.br>
2024-10-11 14:13:36 -0300

    desafio 03 concluído

---
+++ classesDeJogo.js
@@ -0,0 +1,27 @@
+class Heroi {
+  constructor(nome, idade, tipo) {
+    this.nome = nome;
+    this.idade = idade;
+    this.tipo = tipo;
+  }
+
+  atacar() {
+    let ataque = "";
+
+    if (this.tipo === "mago") {
+      ataque = "magia";
+    } else if (this.tipo === "guerreiro") {
+      ataque = "espada";
+    } else if (this.tipo === "monge") {
+      ataque = "artes marciais";
+    } else if (this.tipo === "ninja") {
+      ataque = "shuriken";
+    }
+
+    console.log(`O ${this.tipo} atacou usando ${ataque}`);
+  }
+}
+
+const heroi1 = new Heroi("Lucien", 35, "guerreiro");
+
+heroi1.atacar();
